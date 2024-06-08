096e5d095dfe1243b9e7e2e306f10238b19cf8ed
dogehacker <cl222gs@student.lnu.se>
2024-06-08 12:39:49 +0200

    Updated cppcheck.sh script. Added powershell version. Updated cppcheck.cmake to check operating system before picking bash or powershell script

--- cppcheck/cppcheck.cmake
+++ cppcheck/cppcheck.cmake
@@ -1,6 +1,15 @@
-add_custom_target(YOUR_TARGET
-    COMMAND ./cppcheck.sh
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMENT "Running CppCheck.."
-    VERBATIM
-)
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+	add_custom_target(YOUR_TARGET
+		COMMAND PowerShell -ExecutionPolicy Bypass -File cppcheck.ps1
+		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+		COMMENT "Running CppCheck.."
+		VERBATIM
+	)
+else()
+	add_custom_target(YOUR_TARGET
+		COMMAND /bin/bash cppcheck.sh
+		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+		COMMENT "Running CppCheck.."
+		VERBATIM
+	)
+endif()
